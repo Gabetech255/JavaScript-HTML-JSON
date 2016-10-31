3be91b5e0d9e412c3e9934c42a3b1732432448c2
Gabetech255 <deadpool6969@protonmail.ch>
2016-10-31 05:23:52 -0400

    Add files via upload

---
+++ speeding.js
@@ -0,0 +1,9 @@
+
+var userSpeed = prompt("What is your current speed?");
+var speedLimit = 80;
+if (userSpeed > speedLimit){
+	console.log(userSpeed);
+	console.log("Slow Down");
+}
+else
+	console.log("Your speed is acceptable");
---
+++ uname.js
@@ -0,0 +1,32 @@
+
+//this opens a dialog box at the top of a browser and props the user ok or cancel
+confirm("Would you like to continue with the program");
+
+// prompt opens a dialog box at thee top of the browser and prompts the user to enter something
+
+//creates a variable and saves a string to it
+var userName = prompt("What is your name?");
+var userAge = prompt("What is your age?"); //ints do not have to be in quotations
+
+
+//the console.log function prints to screen 
+console.log("So your name is:");
+console.log(userName);
+console.log("Your age is");
+console.log(userAge);
+
+//.subtring searches a string for a specific slot in memory
+//this particular line changes the value of userName to only the first 3 letters of the name
+userName = userName.substring(0,3);
+
+
+
+//if-else combined with confirm and prompt
+if (confirm("would you like to start the second part of the program?") === true){
+  var myName = prompt("What is your name?");
+  console.log(myName); 
+}
+else
+    console.log("The program either failed or you hit cancel");
+
+
