78d9abef02d2bbda6e45a692d85cc1c8e8d0d0a8
Gabetech255 <deadpool6969@protonmail.ch>
2016-11-02 02:58:11 -0400

    Create hello.js

---
+++ Name-and-Speed/hello.js
@@ -0,0 +1,9 @@
+//This is ment to prompt the user for there name then output it to the screen via html
+function message() {
+
+	var userName = prompt("What is your name?");
+	return userName; 
+
+}
+
+document.write(message());
