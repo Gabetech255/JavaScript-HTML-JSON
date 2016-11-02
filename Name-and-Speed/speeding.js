bec9bc61ef717d24735f3eaeaf1dc3deab295d56
Gabetech255 <deadpool6969@protonmail.ch>
2016-11-02 02:54:41 -0400

    Update speeding.js

--- Name-and-Speed/speeding.js
+++ Name-and-Speed/speeding.js
@@ -1,9 +1,17 @@
+function speeding() 
+{
+	var userSpeed = prompt("What is your current speed?");
+	var speedLimit = 80;
 
-var userSpeed = prompt("What is your current speed?");
-var speedLimit = 80;
-if (userSpeed > speedLimit){
-	console.log(userSpeed);
-	console.log("Slow Down");
+	if (userSpeed > speedLimit) {
+		var msg = "Your doing " + userSpeed + " in an 80 zone. Slow Down";
+		return msg;
+	}
+	else{
+		var msg = "Your speed is fine continue";
+		return msg;
+	}
 }
-else
-	console.log("Your speed is acceptable");
+
+//this calls the function and prints the returned value to the screen in html
+document.write(speeding());
